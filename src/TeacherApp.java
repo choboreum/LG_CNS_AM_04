16344648041d84fd4ac4eb525cfb25daf5ac268a
DESKTOP-9DV92D3\user_boreum <choboreum@naver.com>
2026-01-05 13:05:50 +0900

    객체 생성 테스트

---
+++ src/TeacherApp.java
@@ -0,0 +1,16 @@
+import lgcns.domain.user.Teacher;
+
+public class TeacherApp {
+    public static void main(String[] args) {
+        Teacher teacher = new Teacher();
+
+        // teacher.name = "lim";
+
+        // System.out.println(teacher.name);
+        System.out.println(teacher.name);
+        System.out.println(teacher.age);
+        System.out.println(teacher.mbti);
+
+        
+    }
+}
---
+++ src/lgcns/domain/user/Teacher.java
@@ -0,0 +1,19 @@
+package lgcns.domain.user;
+
+public class Teacher {
+    public String name;
+    public int age;
+    public String mbti;
+    public String birthPlace;
+
+
+    // default constructor
+    public Teacher(){
+        
+    }
+
+    // special constructor
+    public Teacher(String name, int age){
+    
+    }
+}
