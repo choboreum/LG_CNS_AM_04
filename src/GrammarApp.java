32d8d1a7ac1653ee498e8e785a29bdba41d1c9a2
DESKTOP-9DV92D3\user_boreum <choboreum@naver.com>
2026-01-06 14:23:47 +0900

    형변환 테스트

---
+++ src/GrammarApp.java
@@ -0,0 +1,17 @@
+public class GrammarApp {
+    public static void main(String[] args) {
+        
+        // 형변환(casting)
+    
+        byte x = 10, y = 20, z;
+        z = (byte)(x + y);
+        System.out.println(z);
+
+        char a = 'A', b = 'B';
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(a+b);
+
+        float f = 3.12f;
+    }
+}
