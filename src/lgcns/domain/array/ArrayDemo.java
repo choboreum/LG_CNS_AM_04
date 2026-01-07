eb11e9dbfbb93d73de484d2dd7a73b69384e23e8
DESKTOP-9DV92D3\user_boreum <choboreum@naver.com>
2026-01-07 12:50:42 +0900

    chore: gitignore 적용 및 class 파일 정리

--- src/lgcns/domain/array/ArrayDemo.java
+++ src/lgcns/domain/array/ArrayDemo.java
@@ -1,5 +1,23 @@
 package lgcns.domain.array;
 
+import lgcns.domain.user.UserRequestDto;
+
 public class ArrayDemo {
-    
+    private UserRequestDto [] userAry;
+
+    /**
+     * 매개변수로 전달된 데이터를 활용해서 UserRequestDTO 객체를 생성하고
+     * 생성된 객체를 배열에 담는 코드 구현
+     */
+    public void insertTable(String email, String password, String name){
+
+    }
+
+    /**
+     * Quiz)
+     * 생성된 배열 정보를 반환하는 메서드 구현
+     */
+    public UserRequestDto [] getUsers(){ // 요소의 타입이 UserRequestDto인 [](배열)을 반환해라!
+        return userAry;
+    }
 }
