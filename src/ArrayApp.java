8e445e03a3fad11c0958f1ce05b86da70c884694
DESKTOP-9DV92D3\user_boreum <choboreum@naver.com>
2026-01-07 11:21:17 +0900

    배열 이노테이션 기법 적용

--- src/ArrayApp.java
+++ src/ArrayApp.java
@@ -1,3 +1,5 @@
+import lgcns.domain.user.UserResponseDto;
+
 public class ArrayApp {
     public static void main(String[] args) {
         boolean [] ary = new boolean[10];
@@ -11,6 +13,26 @@ public class ArrayApp {
         // enhanced loop
         for(boolean data : ary){ //내부순환자의 문법 - 따로 첨자번지를 작성하지 않고 배열에서 가져와서 돌리도록 함
             System.out.println(data);
+        }
+
+        UserResponseDto userResponseDto = UserResponseDto.builder()
+                                                    .email("lg@lg.cns")
+                                                    .build();
+
+        String email = userResponseDto.getEmail();
+        System.out.println(email);
+
+        UserResponseDto [] userAry = new UserResponseDto[10];
+        userAry[0] = userResponseDto;
+        for(int i = 0; i < userAry.length; i++){
+            if(userAry[i] == null){
+                break;
+            }
+            System.out.println(userAry[i].getEmail());
+        }
+
+        for(UserResponseDto data : userAry){
+            if( data != null ) System.out.println(data.getEmail());
         }
     }
 }
--- src/lgcns/domain/user/UserResponseDto.java
+++ src/lgcns/domain/user/UserResponseDto.java
@@ -1,5 +1,14 @@
 package lgcns.domain.user;
 
+import lombok.Builder;
+import lombok.Getter;
+
+
+@Getter
+@Builder
 public class UserResponseDto {
-    
+    // 배열에 사용자가 입력한 정보들의 묶음인 DTO의 객체를 담기 위해 어노테이션 기법으로 작성
+    private String email, password, name;
+
+
 }
