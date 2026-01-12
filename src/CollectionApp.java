a1175d3fdbfdae1226d211022008686d809f8a2a
DESKTOP-9DV92D3\user_boreum <choboreum@naver.com>
2026-01-12 23:12:37 +0900

    list에 dto담기

--- src/CollectionApp.java
+++ src/CollectionApp.java
@@ -2,6 +2,10 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import lgcns.domain.oop.sub.StudentDTO;
+import lgcns.domain.oop.sub.TeacherDTO;
+import lgcns.domain.oop.sup.PersonDTO;
+
 public class CollectionApp {
     public static void main(String[] args) {
         System.out.println("array");
@@ -26,6 +30,18 @@ public class CollectionApp {
         for(int i = 0; i < list.size(); i++){
             int data = list.get(i);
             System.out.println(data);
+        }
+
+        System.out.println(">>> DTO");
+        List<PersonDTO> perList = new ArrayList<>(); //PersonDTO 타입으로 만든다는 것은, 자식도 함께 쓸 수 있다 => 다형성
+        TeacherDTO teacherDTO = TeacherDTO.builder()
+                                .name("lgcns").age(20).address("seoul").subject("java").build();
+        StudentDTO studentDTO = StudentDTO.builder()
+                                .name("cho").age(24).address("seoul").ssn("2026").build();
+        perList.add(teacherDTO);
+        perList.add(studentDTO);
+        for(PersonDTO data : perList){
+            System.out.println(data.personInfo());
         }
     }
 }
