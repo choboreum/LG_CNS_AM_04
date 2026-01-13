8d597d21ec41ee36b6568301a5cbb2c9193b628c
DESKTOP-9DV92D3\user_boreum <choboreum@naver.com>
2026-01-13 14:41:42 +0900

    title, content, id 입력받아 데이터를 전달하여 int 형으로 값 반환

--- src/lgcns/domain/blog/ctrl/front/FrontCtrl.java
+++ src/lgcns/domain/blog/ctrl/front/FrontCtrl.java
@@ -1,5 +1,6 @@
 package lgcns.domain.blog.ctrl.front;
 
+import lgcns.domain.blog.ctrl.BlogInsertCtrl;
 import lgcns.domain.blog.factory.BlogFactory;
 
 public class FrontCtrl {
@@ -7,5 +8,12 @@ public class FrontCtrl {
 
     public FrontCtrl(){
         factory = BlogFactory.getInstance();
+    }
+
+    public int insert(String requestPath, String title, String content, String writer){
+        System.out.println(">>>> facfory insert");
+        BlogInsertCtrl blogInsertCtrl = (BlogInsertCtrl)factory.getBlogBean(requestPath);
+
+        return blogInsertCtrl.insert(null);
     }
 }
--- src/lgcns/domain/blog/factory/BlogFactory.java
+++ src/lgcns/domain/blog/factory/BlogFactory.java
@@ -1,15 +1,26 @@
 package lgcns.domain.blog.factory;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import lgcns.domain.blog.ctrl.BlogInsertCtrl;
+
 public class BlogFactory {
     private static BlogFactory instance;
-    
-    private BlogFactory(){
+    private Map<String, Object> map;
 
+    private BlogFactory(){
+        map = new HashMap<>();
+        map.put("insert", BlogInsertCtrl.builder().build());
     }
 
     public static BlogFactory getInstance(){
         if(instance == null) instance = new BlogFactory();
 
         return instance;
+    }
+
+    public Object getBlogBean(String requestPath){
+        return map.get(requestPath);
     }
 }
