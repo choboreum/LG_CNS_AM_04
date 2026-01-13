184d620993421b0554b404c44ab56f5726f00076
DESKTOP-9DV92D3\user_boreum <choboreum@naver.com>
2026-01-13 14:09:27 +0900

    Factory에 싱글톤 작업

--- src/lgcns/domain/blog/ctrl/front/FrontCtrl.java
+++ src/lgcns/domain/blog/ctrl/front/FrontCtrl.java
@@ -1,5 +1,11 @@
 package lgcns.domain.blog.ctrl.front;
 
+import lgcns.domain.blog.factory.BlogFactory;
+
 public class FrontCtrl {
-    
+    private BlogFactory factory;
+
+    public FrontCtrl(){
+        factory = BlogFactory.getInstance();
+    }
 }
--- src/lgcns/domain/blog/factory/BlogFactory.java
+++ src/lgcns/domain/blog/factory/BlogFactory.java
@@ -1,5 +1,15 @@
 package lgcns.domain.blog.factory;
 
 public class BlogFactory {
+    private static BlogFactory instance;
     
+    private BlogFactory(){
+
+    }
+
+    public static BlogFactory getInstance(){
+        if(instance == null) instance = new BlogFactory();
+
+        return instance;
+    }
 }
