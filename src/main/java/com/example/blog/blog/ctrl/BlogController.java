6a279c3a055a3793c95e1f1ea5e5414aa41e0ada
DESKTOP-9DV92D3\user_boreum <choboreum@naver.com>
2026-01-22 14:10:47 +0900

    객체 생성 final로 수성

--- src/main/java/com/example/blog/blog/ctrl/BlogController.java
+++ src/main/java/com/example/blog/blog/ctrl/BlogController.java
@@ -12,13 +12,19 @@ import com.example.blog.service.BlogService;
 
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import jakarta.annotation.Resource;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
 @RestController //restApi 통신에서 사용, json
 @RequestMapping(" blogs") //url 패턴 매핑을 통해서 controller을 찾는 과정
+@RequiredArgsConstructor // final로정의된 객체를 생성자로 만들어 받는 구조
 public class BlogController {
     
+    /*
     @Resource(name = "blogService") //name 지정하여 객체 지정을 하여 객체 의존성 주입, @Autowired 대신 사용 가능
     private BlogService blogService;
+    */
+    private final BlogService blogService;
 
     // status code: 201
     @PostMapping("/write") //action 매핑을 통해서 요청 작업을 수행하는 과정
@@ -26,6 +32,7 @@ public class BlogController {
         System.out.println(">>>> BlogController write()" + blogRequestDTO);
 
         // 서비스와 연계
+        blogService.write(blogRequestDTO);
 
         //return new ResponseEntity(HttpStatusCode.OK);
         return new ResponseEntity<>(HttpStatus.OK);
--- src/main/java/com/example/blog/service/BlogService.java
+++ src/main/java/com/example/blog/service/BlogService.java
@@ -6,11 +6,17 @@ import org.springframework.stereotype.Service;
 import com.example.blog.blog.dao.BlogMapper;
 import com.example.blog.blog.domain.dto.BlogRequestDTO;
 
+import lombok.RequiredArgsConstructor;
+
 @Service(value = "blogService")
+@RequiredArgsConstructor
 public class BlogService {
     
+    /*
     @Autowired // 객체 의존성 주입
     private BlogMapper blogMapper;
+    */
+    private final BlogMapper blogMapper;
 
     public void write(BlogRequestDTO blogRequestDTO){
         System.out.println(">>>> BlogService write()");
