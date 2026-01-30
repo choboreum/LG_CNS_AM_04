a3d45df10867fcb8545b6cab91efb8cfdb7a95d8
DESKTOP-9DV92D3\user_boreum <choboreum@naver.com>
2026-01-30 17:00:58 +0900

    토큰 유무에 따라 접근

--- src/main/java/com/example/blog_jpa/BlogJpaApplication.java
+++ src/main/java/com/example/blog_jpa/BlogJpaApplication.java
@@ -3,10 +3,17 @@ package com.example.blog_jpa;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class BlogJpaApplication {
 
 	public static void main(String[] args) {
+		Dotenv env = Dotenv.configure().ignoreIfMissing().load();
+		env.entries().forEach(entry ->
+			System.setProperty(entry.getKey(), entry.getValue())
+		);
+
 		SpringApplication.run(BlogJpaApplication.class, args);
 	}
 
--- src/main/java/com/example/blog_jpa/common/filter/JwtFilter.java
+++ src/main/java/com/example/blog_jpa/common/filter/JwtFilter.java
@@ -1,5 +1,47 @@
 package com.example.blog_jpa.common.filter;
 
-public enum JwtFilter {
+import java.io.IOException;
+
+import org.springframework.stereotype.Component;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class JwtFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+                
+        System.out.println(">>>> JwtFilter doFilter");
+
+        HttpServletRequest req = (HttpServletRequest)request;
+        HttpServletResponse res = (HttpServletResponse)response;
+
+        String endPoint = req.getRequestURI();
+        System.out.println(">>>> JwtFilter User endPoint" + endPoint);
+        String method = req.getMethod();
+        System.out.println(">>>> JwtFilter Request method" + method);
+
+        
+        if ("OPTIONS".equalsIgnoreCase(req.getMethod())) {
+            res.setStatus(HttpServletResponse.SC_OK);
+            res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+            res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+            res.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type");
+            res.setHeader("Access-Control-Allow-Credentials", "true");
+
+            chain.doFilter(request, response);
+            return ;
+        }
+
+        
+    }
     
 }
